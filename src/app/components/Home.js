f1168d44bb35dc398084e60eb8db0e959eaa0d13
최호찬 <starhochan70@gmail.com>
2026-08-05 11:37:08 +0900

    홈 컴포넌트의 구조 수정 및 포트폴리오 링크 업데이트

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home({ data }) {
   return (
-    <>
+    <div className="latest_portfolio">
       <div className="row intro">
         <div className="col-md-4">
           <div className="contents shadow">
@@ -28,7 +28,7 @@ export default function Home({ data }) {
               <div className="hover_contents">
                 <div className="list_info">
                   <h3>
-                    <a href="">{item.title}</a>
+                    <a href={`/portfolio/${item.id}`}>{item.title}</a>
                     <Image src="/images/portfolio_list_arrow.png" width={6} height={8} alt="list arrow" />
                   </h3>
                   <p>
@@ -45,6 +45,6 @@ export default function Home({ data }) {
           See my full portfolio
         </a>
       </p>
-    </>
+    </div>
   );
 }
